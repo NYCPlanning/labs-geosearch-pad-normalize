76d30ac36eb9d564d0ec64eae92309f956074d12
sptkl <caobaiyue@gmail.com>
2021-01-29 18:33:26 +0000

    [build] update push to bucket, dump output to s3 instead of to artifacts

--- push-to-bucket.sh
+++ push-to-bucket.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
+LATEST_PATH=spaces/planninglabs/geosearch-data/latest
+VERSIONED_PATH=spaces/planninglabs/geosearch-data/$VERSION
 
-cd data
-zip labs-geosearch-pad-normalized.zip labs-geosearch-pad-normalized.csv 
-zip labs-geosearch-pad-normalized-sample-lg.zip labs-geosearch-pad-normalized-sample-lg.csv
-zip labs-geosearch-pad-normalized-sample-md.zip labs-geosearch-pad-normalized-sample-md.csv
-zip labs-geosearch-pad-normalized-sample-sm.zip labs-geosearch-pad-normalized-sample-sm.csv
-cd ..
+function upload {
+    mc cp --attr x-amz-acl=public-read $1 $VERSIONED_PATH/$1
+    mc cp --attr x-amz-acl=public-read $1 $LATEST_PATH/$1
+}
 
-s3cmd put data/labs-geosearch-pad-normalized*.zip s3://planninglabs/geosearch-data/
-s3cmd put data/*.json s3://planninglabs/geosearch-data/
-s3cmd setacl s3://planninglabs/geosearch-data/ --acl-public --recursive
+(
+    cd data
+    zip labs-geosearch-pad-normalized.zip labs-geosearch-pad-normalized.csv 
+    upload labs-geosearch-pad-normalized.zip
+
+    zip labs-geosearch-pad-normalized-sample-lg.zip labs-geosearch-pad-normalized-sample-lg.csv
+    upload labs-geosearch-pad-normalized-sample-lg.zip
+
+    zip labs-geosearch-pad-normalized-sample-md.zip labs-geosearch-pad-normalized-sample-md.csv
+    upload labs-geosearch-pad-normalized-sample-md.zip
+
+    zip labs-geosearch-pad-normalized-sample-sm.zip labs-geosearch-pad-normalized-sample-sm.csv
+    upload labs-geosearch-pad-normalized-sample-sm.zip
+)
+
+echo "done"

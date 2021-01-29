91f57134c129e31268f034c33a658fdf71b6ec9b
sptkl <caobaiyue@gmail.com>
2021-01-29 19:05:32 +0000

    [build] change file ownership so that github runner can zip them, with sudo

--- push-to-bucket.sh
+++ push-to-bucket.sh
@@ -9,7 +9,7 @@ function upload {
 
 (
     cd data/nycpad
-    chown $USER:$USER *.csv
+    sudo chown $USER:$USER *.csv
 
     zip labs-geosearch-pad-normalized.zip labs-geosearch-pad-normalized.csv 
     upload labs-geosearch-pad-normalized.zip
